980224d54d08d396ebb742110718cea5b1fe6596
avinasha6 <avinash9999pabdey@gmail.com>
2025-07-04 18:45:47 +0530

    applied toggle to all section

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-const show = document.querySelector(".freqbox");
-const show1 = document.querySelector(".freqbox1");
+// const show = document.querySelector(".freqbox");
+// const show1 = document.querySelector(".freqbox1");
 
-show.addEventListener("click", function () {
+// show.addEventListener("click", function () {
     
-  if (show1.style.display === "none" || show1.style.display === "") {
-    show1.style.display = "block";
-  } else {
-    show1.style.display = "none";
-  }
+//   if (show1.style.display === "none" || show1.style.display === "") {
+//     show1.style.display = "block";
+//   } else {
+//     show1.style.display = "none";
+//   }
+// });
+
+
+const buttons = document.querySelectorAll(".freqbox");
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const answer = btn.nextElementSibling;
+    if (answer.style.display === "none" || answer.style.display === "") {
+      answer.style.display = "block";
+    } else {
+      answer.style.display = "none";
+    }
+  });
 });
+
+
+
+
